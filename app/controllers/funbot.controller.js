59979a6d491cf010730f900e5ce8d2c78fda2978
Jannis Berndt <berndtjannis@gmail.com>
2020-10-09 21:40:26 +0200

    Implemented update function

--- app/controllers/funbot.controller.js
+++ app/controllers/funbot.controller.js
@@ -1,3 +1,36 @@
-exports.update = (req, res) => {
+const quotes = require('./quote.controller.js');
+const fetch = require('node-fetch');
 
+exports.update = async (req, res) => {
+    const payload = req.body;
+    const message = payload.result.message;
+    if(message.text.endsWith("$line")) {
+        const quote = await quotes.getRandomQuote();
+        await fetch(encodeURI(`https://api.telegram.org/bot<bottoken>/sendmessage?chat_id=-408132614&text=${quote.quote}\n ~ ${quote.author} (${quote.title})\nAlbum: ${quote.album}`));
+        res.status(200).send({
+            message: 'Sent line, have fun with it',
+        });
+    } else if(message.text.endsWith("$ir")) {
+        const text = message.text.replace('$ir', '');
+        let result = '';
+        const words = text.split(' ');
+        words.forEach(word => {
+            const letters = word.split('');
+            letters.forEach((letter, i) => {
+                if(i % 2) {
+                    result += letter.toString().toUpperCase();
+                } else {
+                    result += letter.toString().toLowerCase();
+                }
+            });
+            result += ' ';
+        });
+        res.status(200).send({
+            message: result,
+        });
+    } else {
+        res.status(200).send({
+            message: 'Nothing happened',
+        });
+    }
 };
