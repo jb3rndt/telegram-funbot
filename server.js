0902af51238160ea425312970e391b4ee63cd25d
Jannis Berndt <berndtjannis@gmail.com>
2020-10-10 11:40:24 +0200

    removing old MongoDB stuff

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const config = require('./config.js');
-const mongoose = require('mongoose');
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -29,16 +28,6 @@ pool.query(createTableText).then(() => {
 }).catch((err) => {
     console.log(err);
 });
-// mongoose.Promise = global.Promise;
-
-// mongoose.connect(config.db_url, {
-//     useNewUrlParser: true
-// }).then(() => {
-//     console.log('Successfully connected to the database');
-// }).catch(err => {
-//     console.log('Could not connect to the database. Exiting now...', err);
-//     process.exit();
-// })
 
 app.get('/', (req, res) => {
     res.json({'message': 'Welcome to the funny Telegram Bot API'});
